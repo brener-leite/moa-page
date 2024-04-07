feb5b5cebc1a6b660ef5c4d29881590014797506
Brener Leite <brenerdealves@gmail.com>
2024-04-07 17:08:33 -0300

    refactor: adjust mobile version features section

--- src/pages/Home/sections/Features/index.tsx
+++ src/pages/Home/sections/Features/index.tsx
@@ -12,16 +12,28 @@ function Features() {
           src="/assets/daily-run-section.png"
           alt="Person running"
           loading="lazy"
+          className="hidden lg:block order-1"
         />
-        <Card.Container>
+        <img
+          src="/assets/daily-run-section-mobile.png"
+          alt="Person running"
+          loading="lazy"
+          className="block lg:hidden order-2"
+        />
+        <Card.Container className="order-1 lg:order-2">
           <Card.Light position="left" />
           <div className="mb-6">
-            <Card.Title>Daily Run</Card.Title>
+            <Card.Title>
+              <span className="text-pink-1 font-semibold">Daily Runs</span>
+            </Card.Title>
           </div>
           <Card.Text>
-            Lorem Lorem ipsum dolor ipsum dolor{" "}
-            <span className="text-pink-1">sit amet</span> Lorem ipsum dolor sit
-            amet consectetur. Esvul morbi sit vitae. Aet et a tempor
+            Start your day on the right foot with Moa's Daily Runs.{" "}
+            <span className="text-pink-1 font-semibold">
+              Challenge yourself
+            </span>{" "}
+            to personal bests, earn tokens, and stay motivated as you track your
+            progress every step of the way.
           </Card.Text>
         </Card.Container>
       </Section.Row>
@@ -29,18 +41,30 @@ function Features() {
         <Card.Container>
           <Card.Light position="right" />
           <div className="mb-6">
-            <Card.Title>Challenges</Card.Title>
+            <Card.Title>
+              <span className="text-pink-1 font-semibold">Challenges</span>
+            </Card.Title>
           </div>
           <Card.Text>
-            Lorem Lorem ipsum dolor ipsum dolor{" "}
-            <span className="text-pink-1">sit amet</span> Lorem ipsum dolor sit
-            amet consectetur. Esvul morbi sit vitae. Aet et a tempor
+            Get ready to push your limits with Moa's Challenges.{" "}
+            <span className="text-pink-1 font-semibold">
+              Join or create custom runs
+            </span>
+            , set your own rules, and compete against friends or the global
+            community for rewards and bragging rights.
           </Card.Text>
         </Card.Container>
         <img
           src="/assets/challenge-run-section.png"
           alt="Person with hat medidating"
           loading="lazy"
+          className="hidden lg:block"
+        />
+        <img
+          src="/assets/challenge-run-section-mobile.png"
+          alt="Person with hat medidating"
+          loading="lazy"
+          className="block lg:hidden"
         />
       </Section.Row>
       <Section.Row>
@@ -48,16 +72,29 @@ function Features() {
           src="/assets/event-run-section.png"
           alt="Map with pins"
           loading="lazy"
+          className="hidden lg:block order-1"
         />
-        <Card.Container>
+        <img
+          src="/assets/event-run-section-mobile.png"
+          alt="Map with pins"
+          loading="lazy"
+          className="block lg:hidden order-2"
+        />
+        <Card.Container className="order-1 lg:order-2">
           <Card.Light position="left" />
           <div className="mb-6">
-            <Card.Title>Global Events</Card.Title>
+            <Card.Title>
+              <span className="text-pink-1 font-semibold">Global Events</span>
+            </Card.Title>
           </div>
           <Card.Text>
-            Lorem Lorem ipsum dolor ipsum dolor{" "}
-            <span className="text-pink-1">sit amet</span> Lorem ipsum dolor sit
-            amet consectetur. Esvul morbi sit vitae. Aet et a tempor
+            Experience the excitement of running on a global scale with Moa's
+            Global Events.{" "}
+            <span className="text-pink-1 font-semibold">
+              Compete against runners from around the world
+            </span>
+            , earn prestigious rewards, and be part of something truly
+            extraordinary.
           </Card.Text>
         </Card.Container>
       </Section.Row>
